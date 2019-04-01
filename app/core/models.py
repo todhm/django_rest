808419c1c3dfaf0979af434bfc283eb3b8f11ac2
todhm <todhm@nate.com>
2019-04-02 06:51:32 +0900

    add update api

--- app/core/models.py
+++ app/core/models.py
@@ -70,7 +70,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5,decimal_places=2)
     link = models.CharField(max_length=255,blank=True)
-    ingredeints = models.ManyToManyField('Ingredient')
+    ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
 
 
